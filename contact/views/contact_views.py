b0a20ae931895a0c2a151a0cc50ab5662d55e0f2
Leandro <leandrotomasetto@gmail.com>
2025-05-20 18:12:11 -0300

    Configurando o search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,17 +1,42 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-
+from django.shortcuts import render, get_object_or_404, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')[10:20]
 
-    print(contacts.query)
-
     context = {
         'contacts': contacts,
         'site_title': 'Contatos - ',
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context,
+    )
+
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+    
+    contacts = Contact.objects\
+    .filter(show=True)\
+    .filter(
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value) 
+    )\
+    .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
     }
 
     return render(
