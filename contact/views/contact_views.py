1ca5189343440acf930746db17cd46def58e43c6
Leandro <leandrotomasetto@gmail.com>
2025-05-20 17:24:40 -0300

    Colocando os contacts no site e configurando outra URL

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
 from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')[10:20]
+
+    print(contacts.query)
 
     context = {
         'contacts': contacts,
@@ -12,5 +16,20 @@ def index(request):
     return render(
         request,
         'contact/index.html',
+        context,
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
         context,
     )
