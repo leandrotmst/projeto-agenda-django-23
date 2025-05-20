7d6082bbb1c0d9e03fc031a28038b7d94a33b755
Leandro <leandrotomasetto@gmail.com>
2025-05-20 20:03:09 -0300

    configurando errors

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,24 +1,47 @@
+from typing import Any
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Q
+from django import forms
+from django.core.exceptions import ValidationError
+
 from contact.models import Contact
 
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact 
+        fields = (
+            'first_name', 'last_name', 'phone',
+        )    
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Mensagem de erro',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
 
 def create(request):
     if request.method == 'POST':
-        print()
-        print(request.method)
-        print(request.POST.get('first_name'))
-        print(request.POST.get('last_name'))
-        print()
+        context = {
+            'form': ContactForm(data=request.POST)
+        }
+
+        return render(
+            request,
+            'contact/create.html',
+            context,
+        )
 
     context = {
-
+        'form': ContactForm()
     }
-
-    print()
-    print(request.method)
-    print()
 
     return render(
         request,
