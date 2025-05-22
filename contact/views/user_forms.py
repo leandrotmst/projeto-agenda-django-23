a9e1d876d3fa831e7fce9086dad94183d73af5c4
Leandro <leandrotomasetto@gmail.com>
2025-05-22 17:09:01 -0300

    Validando password

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 
 
 def register(request):
@@ -23,6 +23,44 @@ def register(request):
             'form': form,
         }
     )
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+    if request.method == "POST":
+        form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            auth.login(request, request.user)  # New Login
+            messages.success(request, "Atualizado com sucesso.")
+            return redirect("contact:user_update")
+
+    return render(
+        request,
+        "contact/user-update.html",
+        {
+            "site_title": "Update User - ",
+            "form": form,
+        },
+    )
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/register.html',
+            {
+                'form': form,
+            }
+        )
+    
+    form.save()
+    return render(
+            request,
+            'contact/register.html',
+            {
+                'form': form,
+            }
+        )
 
 def login_view(request):
     form = AuthenticationForm(request)
