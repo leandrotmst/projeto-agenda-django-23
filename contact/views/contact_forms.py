2901749e64c75d9d4aca17215a30c681a9bac132
Leandro <leandrotomasetto@gmail.com>
2025-05-26 14:42:28 -0300

    Projeto completo Agenda Django

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from contact.forms import ContactForm
 from contact.models import Contact
 
+
 @login_required(login_url='contact:login')
 def create(request):
     form_action = reverse('contact:create')
@@ -18,7 +19,9 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
+            contact.save()
             return redirect('contact:update', contact_id=contact.pk)
 
         return render(
@@ -41,7 +44,7 @@ def create(request):
 @login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
     form_action = reverse('contact:update', args=(contact_id,))
 
@@ -77,7 +80,7 @@ def update(request, contact_id):
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
-        Contact, pk=contact_id, show=True
+        Contact, pk=contact_id, show=True, owner=request.user
     )
     confirmation = request.POST.get('confirmation', 'no')
 
